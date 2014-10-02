e2eafdf8df89327c2e17f330295b394fdc442cc6
86.marcao@gmail.com <86.marcao@gmail.com@615689d6-7de3-eee8-3512-48c3a5262af6>
2014-10-02 16:40:56 +0000

    Colocar as funcoes javascript de caminho do usuario no arquivo central mapa.

--- smc-web/WebContent/resources/js/mapa.js
+++ smc-web/WebContent/resources/js/mapa.js
@@ -207,6 +207,139 @@ function addMarkerPorUltimaLocalizacao(location, i, icone, idUsuario, title, act
 	})(marker, i));
 }
 
+function caminhoUsuario(json) {
+	// Caso venha vazio nao faz nada
+	if (json == null || json == '') {
+		return;
+	}
+
+	var directionsService = new google.maps.DirectionsService({
+		//avoidHighways: true
+	});
+
+	var directionsDisplay = new google.maps.DirectionsRenderer({
+		suppressMarkers: true,
+		suppressInfoWindows: true
+	});
+
+	var myOptions = {
+		zoom: 5,
+		center: latlon,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	// Create a Map Object
+	map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+
+	// Relacionamos o directionsDisplay com o mapa desejado
+	directionsDisplay.setMap(map);
+
+
+	var parsedJSON = $.parseJSON(json);
+
+	var origin, destination, usuario;
+	var waypoints = [];
+
+	for (var i in parsedJSON) {
+		var latLng = new google.maps.LatLng(parsedJSON[i].latitude, parsedJSON[i].longitude);
+		var icone = parsedJSON[i].icone;
+		var idUsuario = parsedJSON[i].idUsuario;
+		var title = parsedJSON[i].detalhe;
+		var status = "active";
+
+		addMarkerCaminhoUsuario(latLng, i, icone, idUsuario, title, status);
+
+		// Salvar os dados para criar a rota
+		if (i == 0) {
+			destination = latLng;
+			usuario = title;
+		}
+		waypoints.push({
+			location: latLng
+		});
+		origin = latLng;
+	}
+
+	//Apos o for, remover o ultimo elemento do array, pois ele e o ponto de origem.
+	waypoints.pop();
+
+	var request = {
+		origin: origin,
+		destination: destination,
+		waypoints: waypoints,
+		//travelMode: google.maps.TravelMode.DRIVING // indica rotas de trânsito padrão usando a rede rodoviária.
+		travelMode: google.maps.TravelMode.WALKING // solicita rotas a pé por faixas de pedestre e calçadas.
+	};
+
+	directionsService.route(request, function(result, status) {
+		if (status == google.maps.DirectionsStatus.OK) { // Se deu tudo certo
+			directionsDisplay.setDirections(result); // Renderizamos no mapa o resultado
+			//showSteps(result, usuario);
+		} else {
+			alert("Problema ao carregar a rota, status: "+status);
+		}
+	});
+
+	/*function showSteps(directionResult, usuario) {
+		// For each step, place a marker, and add the text to the marker's
+		// info window. Also attach the marker to an array so we
+		// can keep track of it and remove it when calculating new
+		// routes.
+		var myRoute = directionResult.routes[0].legs[0];
+
+		for (var i = 0; i &lt; myRoute.steps.length; i++) {
+			var marker = new google.maps.Marker({
+				position: myRoute.steps[i].start_point,
+				map: map
+			});
+			attachInstructionText(marker, usuario + " -> " + myRoute.steps[i].instructions);
+			markerArray[i] = marker;
+		}
+	}
+
+	function attachInstructionText(marker, text) {
+		google.maps.event.addListener(marker, 'click', function() {
+			stepDisplay.setContent(text);
+			stepDisplay.open(map, marker);
+		});
+	} */
+}
+
+function addMarkerCaminhoUsuario(location, i, icone, idUsuario, title, active) {
+	var marker = new google.maps.Marker({
+		position: location,
+		draggable: true,
+		icon: icone,
+		// title:"Você está Aqui!",
+        status: active,
+		map: map
+	});
+
+	// Adicionar informacao no marcador ao passar o mouse em cima
+	google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+		return function() {
+			infowindow.setContent(title);
+			infowindow.open(map, marker);
+		}
+	})(marker, i));
+
+	// Remover informacao do marcador ao retirar o mouse de cima
+	google.maps.event.addListener(marker, 'mouseout', (function(marker, i) {
+		return function() {
+			infowindow.close(map, marker);
+		}
+	})(marker, i));
+
+	// Evento de clicar
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		return function() {
+			alert("Cliquei");
+			//infowindow.setContent(usuario);
+			//infowindow.open(map, marker);
+		}
+	})(marker, i));
+}
+
 function showError(error) {
 	switch (error.code) {
 		case error.PERMISSION_DENIED:
