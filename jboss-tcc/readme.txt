fd74698950b6c90baa6aaffb9192f736b952255c
86.marcao@gmail.com <86.marcao@gmail.com@615689d6-7de3-eee8-3512-48c3a5262af6>
2015-02-21 12:50:09 +0000

    Renomear projeto de 'smc' para 'sgc'.

--- jboss-tcc/readme.txt
+++
@@ -1,2 +0,0 @@
-This folder is the default location configured in the Runtime Detection Plug-in, which 
-checks this location for new runtimes each time Red Hat JBoss Developer Studio is started.
--- smc-ejb-client/ejbModule/br/com/baraunatecnologia/smc/ejb/interfaces/IMensagemLocal.java
+++
@@ -1,9 +0,0 @@
-package br.com.baraunatecnologia.smc.ejb.interfaces;
-import javax.ejb.Local;
-
-import br.com.baraunatecnologia.smc.ejb.entity.Mensagem;
-
-@Local
-public interface IMensagemLocal extends ICRUDBean<Mensagem>{
-
-}
