6d160d660bd848e846bd541ee7d8022691e1e665
86.marcao@gmail.com <86.marcao@gmail.com@615689d6-7de3-eee8-3512-48c3a5262af6>
2015-02-21 12:52:27 +0000

    Renomear projeto de 'smc' para 'sgc'.

--- smc-ejb-client/ejbModule/br/com/baraunatecnologia/smc/ejb/interfaces/IMensagemRemote.java
+++
@@ -1,9 +0,0 @@
-package br.com.baraunatecnologia.smc.ejb.interfaces;
-import javax.ejb.Remote;
-
-import br.com.baraunatecnologia.smc.ejb.entity.Mensagem;
-
-@Remote
-public interface IMensagemRemote extends ICRUDBean<Mensagem> {
-
-}
